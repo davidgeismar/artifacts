edbbbc076dbc9eb80d1de088344febf3170b6b0d
David Geismar <davidgeismar@hotmail.fr>
2020-03-21 12:06:56 +0000

    changed bas api

--- app/javascript/packs/env.js
+++ app/javascript/packs/env.js
@@ -2,5 +2,5 @@ export let baseDataApiURI;
 if (process.env.RAILS_ENV == 'development') {
   baseDataApiURI = 'http://localhost:3000'
 } else {
-  baseDataApiURI = 'http://35.180.85.250:3000'
+  baseDataApiURI = `${window.location.origin}:3000`
 }
